385f7272123537f8e939df77568a6e51d1a6da1c
mopanowicz <marek.opanowicz@gmail.com>
2023-04-14 15:50:10 +0200

    Update response

--- functions/get-all/index.js
+++ functions/get-all/index.js
@@ -1,13 +1,18 @@
+const response = [{
+    "id": 1,
+    "name": "John Doe",
+    "email": "john.doe@wp.pl"
+},{
+    "id": 2,
+    "name": "Katerina Pavelkova",
+    "email": "k.pav@nevi.cz"
+}]
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    context.log('Response: ' + response);
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+        body: response
+    }; 
 }
